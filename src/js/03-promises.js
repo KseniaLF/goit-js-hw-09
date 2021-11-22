29dccde78a7d9c685f66e7cedd09c7737d6faa2f
KseniaLF <llf9391@gmail.com>
2021-11-22 17:23:04 +0200

    1-3дз

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const refs = {
+    form: document.querySelector(".form"),
+}
+
+refs.form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    let delayValue = Number(event.target.delay.value);
+    const stepValue = Number(event.target.step.value);
+    const amountValue = Number(event.target.amount.value);
+
+
+    for (let i = 1; i <= amountValue; i++) {
+        createPromise(i, delayValue)
+            .then(({ position, delay }) => {
+                console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+        delayValue += stepValue;
+    }
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({position, delay});
+            } 
+                reject({position, delay});
+        }, delay)
+    })
 }
